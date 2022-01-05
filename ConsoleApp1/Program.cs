03664b19dca7477628c591f86dfae1b31dfee862
Krzysztof <Krzysztofbojarczuk@wp.pl>
2022-01-05 08:33:27 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+while (true)
+{
+
+    try
+    {
+
+   
+    Console.WriteLine("Wpisz liczbę do której ma być wyliczna lista: ");
+    int user = int.Parse(Console.ReadLine());
+
+    for (int i = 0; i < user; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            Console.WriteLine($"{i} FizzBuzz");
+        }
+        else if (i % 3 == 0)
+        {
+            Console.WriteLine($"{i} Fizz");
+        }
+        else if (i % 5 == 0)
+        {
+            Console.WriteLine($"{i} Buzz");
+        }
+        else
+        {
+            Console.WriteLine(i);
+        }
+    }
+    Console.WriteLine("Czy chesz zakończyć program? Jeśtli tak wpisz Tak/TAK Jesli nie wciśjnie dolwolny przycisk");
+    string lol = Console.ReadLine();
+    if (lol == "TAK" || lol == "Tak" || lol == "TAk" || lol == "tak")
+    {
+        break;
+    }
+    }
+    catch (Exception ex)
+    {
+
+        Console.WriteLine(ex.Message);
+    }
+
+}
